3be3fb604bb36c3aa2ebb7ab0c0265c1f3a7b1c3
Kathy Kanemoto <kanemoto.k@mccd.edu>
2021-08-04 12:05:10 -0700

    added some comments and todo items

--- src/cropDroneAnalysis/AnalysisProgram.java
+++ src/cropDroneAnalysis/AnalysisProgram.java
@@ -1,10 +1,23 @@
+/**************************************************\
+ * Crop Drone Analysis
+ * 
+ * write intro stuff here
+ * 
+ * 
+ * last updated 8.4.2021
+ \*************************************************/
+
 package cropDroneAnalysis;
 
 public class AnalysisProgram {
+	
+	//data variables
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//write some test output to test the methods
 
 	}
+	
+	//methods for calculations
 
 }
